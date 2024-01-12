44d64a8a82d392941fe3724737532b9119fe4471
idamakela <idamakela98@gmail.com>
2024-01-12 11:01:07 +0100

    feat: created reusable btn omponent

---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+import { VariantProps, cva } from 'class-variance-authority'
+
+import { cn } from '@/utils/classnames'
+
+export const buttonVariants = cva(
+  'p-3 md:p-5 font-semibold transition duration-300 border-transparent uppercase',
+  {
+    variants: {
+      variant: {
+        filled: 'bg-black text-white hover:bg-white hover:text-black border-2 hover:border-black',
+        ghost: 'text-black p-0 border-b-2 hover:border-black',
+        outline: 'bg-none text-black border-2 border-black hover:bg-black hover:text-white',
+      },
+    },
+    defaultVariants: {
+      variant: 'outline',
+    },
+  },
+)
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants>
+
+const Button = ({ children, variant, className, ...props }: ButtonProps) => {
+  return (
+    <button className={cn(buttonVariants({ variant, className }))} {...props}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
