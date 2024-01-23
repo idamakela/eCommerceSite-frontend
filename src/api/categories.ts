45f2605a793388c326fa5d7c9b5e3569776638ea
idamakela <idamakela98@gmail.com>
2024-01-23 14:56:54 +0100

    chore: cleaned unused code

--- src/api/categories.ts
+++ src/api/categories.ts
@@ -1,16 +1,11 @@
 'use server'
+
 import axios from 'axios'
 import { categoriesEndpoint } from './endpoints'
-import { CategoryAndSubcategoryRes } from '@/utils/types'
 
 const storeApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_STRAPI_API_URL,
 })
-
-export const getCategories = async (): Promise<CategoryAndSubcategoryRes> => {
-  const response = await storeApi.get<CategoryAndSubcategoryRes>(categoriesEndpoint)
-  return response.data
-}
 
 export const getNavigation = async () => {
   try {
