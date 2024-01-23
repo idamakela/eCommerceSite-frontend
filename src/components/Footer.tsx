564cc999a9cefc636300d5c94f19122410bf9c29
idamakela <idamakela98@gmail.com>
2024-01-23 16:47:20 +0100

    refactor: Footer and header fetched data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,22 @@ import Link from 'next/link'
 import Logo from './Logo'
 import { getFooter } from '@/api/single-types'
 
+type DataRes = {
+  data: {
+    attributes: {
+      title: string
+      desc: string
+    }
+  }
+}
+
 const Footer = async () => {
-  const data = await getFooter()
+  const { data }: DataRes = await getFooter()
 
   return (
     <footer className='flex flex-col items-center bg-neutral-400 px-3'>
-      <h3 className='pb-5 pt-14 font-header font-medium uppercase'>
-        {data?.data?.attributes?.title}
-      </h3>
-      <p className='text-center text-xs'>{data?.data?.attributes?.desc}</p>
+      <h3 className='pb-5 pt-14 font-header font-medium uppercase'>{data?.attributes.title}</h3>
+      <p className='max-w-[530px] text-center text-xs'>{data?.attributes.desc}</p>
       <div className='pb-14 pt-28'>
         <Link href='/'>
           <Logo />
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,19 +2,35 @@ import Image from 'next/image'
 
 import { getLogo } from '@/api/single-types'
 
+type DataRes = {
+  data: {
+    attributes: {
+      name: string
+      img: {
+        data: {
+          attributes: {
+            url: string
+            mime: string
+          }
+        }
+      }
+    }
+  }
+}
+
 const Logo = async () => {
-  const data = await getLogo()
+  const { data }: DataRes = await getLogo()
 
   return (
     <div className='flex w-fit items-center justify-center gap-2'>
       <Image
-        src={data?.data?.attributes?.img.data.attributes.url}
+        src={data?.attributes.img.data.attributes.url}
         alt='logo'
         width={24}
         height={24}
-        blurDataURL={data?.data?.attributes?.img.data.attributes.mime}
+        blurDataURL={data?.attributes.img.data.attributes.mime}
       />
-      <h2 className='font-header font-semibold uppercase'>{data?.data?.attributes?.name}</h2>
+      <h2 className='font-header font-semibold uppercase'>{data?.attributes.name}</h2>
     </div>
   )
 }
